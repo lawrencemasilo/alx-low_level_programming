138ce1650de9fe87a3fc40ad499491bb48c0603d
Neo Masilo <neolawrencemasilo@gmail.com>
2023-03-16 19:25:46 +0000

    101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'This code prnts code using stderr'
+ *
+ * @void: 'no type'
+ *
+ * Return: 1 (unsuccessful)
+ */
+
+int main(void)
+{
+	char x = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
+	fprintf(stderr, "x = %d\n", x);
+
+	return (1);
+}

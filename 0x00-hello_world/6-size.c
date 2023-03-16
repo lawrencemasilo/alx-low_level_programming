76908e73df314a0b4344fcbba43f36442d5326da
Neo Masilo <neolawrencemasilo@gmail.com>
2023-03-16 18:31:48 +0000

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'This program prints data type sizes'
+ *
+ * @void: 'no type'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("Size of a char: %zu byte(s)", sizeof(char));
+	printf("Size of an int: %zu byte(s)", sizeof(int));
+	printf("Size of a long int: %zu byte(s)", sizeof(long));
+	printf("Size of a long long int: %zu byte(s)", sizeof(long long));
+	printf("Size of a float: %zu byte(s)", sizeof(float));
+
+	return (0);
+}

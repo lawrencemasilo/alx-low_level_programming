3d3577f0a2e3048e1fcb629f5659c2c12c49c17b
Neo Masilo <neolawrencemasilo@gmail.com>
2023-03-16 18:03:25 +0000

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'this is a program that prints text using printf'
+ *
+ * @void: 'no type'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+
+	return (0);
+}

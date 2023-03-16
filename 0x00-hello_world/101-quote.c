0e85487760a1ba382f0959559eed1b04c5af2706
Neo Masilo <neolawrencemasilo@gmail.com>
2023-03-16 19:50:28 +0000

    101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -12,9 +12,7 @@
 
 int main(void)
 {
-	char x = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-
-	fprintf(stderr, "x = %d\n", x);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
 	return (1);
 }

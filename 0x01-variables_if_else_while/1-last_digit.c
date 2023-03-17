fbba054ea50d3128e532df29e9e7974f42374a84
Neo Masilo <neolawrencemasilo@gmail.com>
2023-03-17 22:44:31 +0000

    last digit task

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,32 +4,28 @@
 
 /**
  * main - Entry point
- *
  * Return: Always 0 (Successful)
  */
 
 int main(void)
 {
 	int n;
+	int la_digit;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	int last_digit;
-	last_digit = n % 10;
-
-	printf("Last digit of %d is ", n);
-
-	if (last_digit > 5)
+	la_digit = n % 10;
+	if (la_digit > 5)
 	{
-		printf("%d and is greater than 5\n", last_digit);
+		printf("Last digit of %d is %d and is greater than 5\n", n, la_digit);
 	}
-	else if (last_digit == 0)
+	else if (la_digit == 0)
 	{
-		printf("%d and is 0\n", last_digit);
+		printf("Last digit of %d is %d and is 0\n", n, la_digit);
 	}
 	else
 	{
-		printf("%d and is less than 6 and not 0\n", last_digit);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, la_digit);
 	}
 	return (0);
 }

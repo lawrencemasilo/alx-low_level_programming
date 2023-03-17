38f7ed797ab72ffe1fb792894db1407f014cb5e8
Neo Masilo <neolawrencemasilo@gmail.com>
2023-03-17 23:08:32 +0000

    last digit task

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: 'This program prints out the alphabet'
+ * Return: 0 (successful)
+ */
+
+int main(void)
+{
+	int a;
+	for (a = 65; a <= 90; a++)
+	{
+		putchar(a);
+	}
+	return (0);
+}

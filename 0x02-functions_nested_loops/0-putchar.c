d071af43391f93beba185f2f94006001cc5528b4
Neo Masilo <neolawrencemasilo@gmail.com>
2023-03-21 17:37:10 +0000

    first task update

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,24 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
  * main - 'Entry point'
- * Description: 'This program outputs the following "_putchar"'
+ * Description: 'This program outputs the following word "_putchar"'
  * @void: 'no type'
- * Return: 0 (success)
+ * Return: Always 0 (successful)
  */
 
 int main(void)
 {
-	char a = "_putchar";
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
-	putchar("%d\n", a);
 	return (0);
 }

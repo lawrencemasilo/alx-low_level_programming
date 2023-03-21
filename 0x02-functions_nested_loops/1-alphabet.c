77617362e16ff48daf98dca55dde71f5c16df49d
Neo Masilo <neolawrencemasilo@gmail.com>
2023-03-21 19:17:51 +0000

    alphabet task

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - 'This programs entry function'
+ * @void: 'no type'
+ * no return value
+ */
+
+void print_alphabet(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		_putchar(a);
+	}
+}

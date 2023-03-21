0c9da24003644501166bcafabb16d0e564e0d31f
Neo Masilo <neolawrencemasilo@gmail.com>
2023-03-21 19:56:40 +0000

    print alphabet 10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - 'is a funtion that outputs the  alphabet 10x'
+ * @void: 'no type'
+ */
+
+void print_alphabet_x10(void)
+{
+	int i = 1;
+
+	while (i <= 10)
+	{
+		print_alphabet();
+		putchar('\n');
+		i++;
+	}
+	_putchar('\n');
+}

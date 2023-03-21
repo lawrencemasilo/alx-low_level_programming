6190a6a17bf4a48b7e1c5763862c128ca8f98ce1
Neo Masilo <neolawrencemasilo@gmail.com>
2023-03-21 16:33:58 +0000

    Functions

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - 'Entry point'
+ * Description: 'This program outputs the following "_putchar"'
+ * @void: 'no type'
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	char a = "_putchar";
+
+	putchar("%d\n", a);
+	return (0);
+}

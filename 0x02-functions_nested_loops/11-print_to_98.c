314318e795d913568d43855a7b1d00739408094b
Neo Masilo <neolawrencemasilo@gmail.com>
2023-03-22 11:36:57 +0000

    n to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,17 +9,18 @@
 void print_to_98(int n)
 {
 	printf("%d", n);
+	int i;
 
 	if (n < 98)
 	{
-		for (int i = n + 1; i <= 98; i++)
+		for (i = n + 1; i <= 98; i++)
 		{
 			printf(", %d", i);
 		}
 	}
 	else
 	{
-		for (int i = n - 1; i >= 98; i--)
+		for (i = n - 1; i >= 98; i--)
 		{
 			printf(", %d", i);
 		}

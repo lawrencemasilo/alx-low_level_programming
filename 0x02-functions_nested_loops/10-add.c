210668ce1aa9731d0759aa1c78d80cff38e6a996
Neo Masilo <neolawrencemasilo@gmail.com>
2023-03-22 10:19:43 +0000

    task 10, updates to main.h file

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - sum up two integers
+ * @a: are the argument for function
+ * @b: are the argument for function
+ * Return: 0 (successful)
+ */
+
+int add(int a, int b)
+{
+	sum = a + b;
+	_putchar(sum);
+	return (0);
+}

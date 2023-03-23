3de910b857639c9bfea8856606d1ee2839f566ee
Neo Masilo <neolawrencemasilo@gmail.com>
2023-03-23 07:14:34 -0700

    The largest number

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -16,7 +16,11 @@ int largest_number(int a, int b, int c)
 	{
 		largest = a;
 	}
-	else if (b > a && b > c)
+	else if (a > b && c > a)
+	{
+		largest = c;
+	}
+	else if (b > c)
 	{
 		largest = b;
 	}
@@ -25,6 +29,5 @@ int largest_number(int a, int b, int c)
 		largest = c;
 	}
 
-	printf("\n");
 	return (largest);
 }

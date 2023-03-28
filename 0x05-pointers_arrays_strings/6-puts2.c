04b7553b143dc3e21f45816b7425159640e1c0d1
Neo Masilo <neolawrencemasilo@gmail.com>
2023-03-28 06:18:18 -0700

    half the truth task

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * puts2 - This function prints every other character
+ * @str: string argument
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i]; i += 2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}

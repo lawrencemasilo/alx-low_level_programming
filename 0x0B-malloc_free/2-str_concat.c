324b89789dd7e5c5ee4094c143e5a4a874b78b3c
Neo Masilo <neolawrencemasilo@gmail.com>
2023-04-12 14:41:24 +0000

    concatenate

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -19,10 +19,12 @@ char *str_concat(char *s1, char *s2)
 	if (s1 == NULL)
 	{
 		s1 = " ";
+		return (s2);
 	}
 	if (s2 == NULL)
 	{
-		s1 = " ";
+		s2 = " ";
+		return (s1);
 	}
 	while (s1[len1] != '\0')
 	{

075be507c681445642ce443246bf0093dba6d300
Neo Masilo <neolawrencemasilo@gmail.com>
2023-04-12 18:49:20 +0000

    2d array

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -23,14 +23,25 @@ int **alloc_grid(int width, int height)
 		return (NULL);
 	}
 	a = malloc(height * sizeof(int *));
+
+	if (a == NULL)
+	{
+		return (NULL);
+	}
+
 	for (i = 0; i < height; i++)
 	{
 		a[i] = malloc(width * sizeof(int));
 		if (a[i] == NULL)
 		{
-			return (NULL);
+			for (j = 0; j < i; j++)
+			{
+				free(a[j]);
+			}
+			free(a);
 		}
 	}
+
 	for (j = 0; j < width; j++)
 	{
 		for (i = 0; i < height; i++)

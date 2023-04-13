d2475157f3d930cd54a3076c7a58a5652b3a8e3e
Neo Masilo <neolawrencemasilo@gmail.com>
2023-04-13 21:41:06 +0000

    allocate

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,7 +11,8 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *a;
+	unsigned int *a;
+	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
 	{
@@ -21,6 +22,10 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (a == NULL)
 	{
 		return (NULL);
+	}
+	for (i = 0; i < nmemb; i++)
+	{
+		a[i] = 0;
 	}
 	return (a);
 }

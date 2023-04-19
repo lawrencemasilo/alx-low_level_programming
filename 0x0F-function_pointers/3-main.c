944e5f133a996270c81a7696bf4d2941bc936c55
Neo Masilo <neolawrencemasilo@gmail.com>
2023-04-19 14:50:14 -0700

    last task

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,38 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main function of  the program
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: 0 (successful)
+ */
+
+int main(int argc, char *argv[])
+{
+	int num1, num2, equ;
+	int (*func)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	func = get_op_func(argv[2]);
+	if (func == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if  ((argv[2][0] == '%' || argv[2][0] == '/') && num2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	equ = func(num1, num2);
+	printf("%d\n", equ);
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,56 @@
+#include "3-calc.h"
+#include <stdio.h>
+/**
+ * op_add - returns sum of numbers
+ * @a: first number
+ * @b: second number
+ * Return: a + b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - returns difference of numbers
+ * @a: first number
+ * @b: second number
+ * Return: a - b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - returns product of numbers
+ * @a: first number
+ * @b: second number
+ * Return: a * b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - returns division of numbers
+ * @a: first number
+ * @b: second number
+ * Return: a / b
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - returns division reminder of numbers
+ * @a: first number
+ * @b: second number
+ * Return: a % b
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}

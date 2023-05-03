953429263f0470299cf2e7f5307b1a068057cb7f
Neo Masilo <neolawrencemasilo@gmail.com>
2023-05-03 15:49:56 -0700

    insert node

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -13,7 +13,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int i = 0;
 	listint_t *node = NULL, *nd;
 
-	if (*head == NULL)
+	if (head == NULL)
 	{
 		return (NULL);
 	}
@@ -22,7 +22,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		nd = add_nodeint_end(head, n);
 		return (nd);
 	}
-	while (*head != NULL && i < (idx - 1))
+	while (*head != NULL && i < idx - 1)
 	{
 		*head = (*head)->next;
 		i++;

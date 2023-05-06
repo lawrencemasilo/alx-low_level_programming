c794025f65dfcec3a5475da3cc7368063136f2a3
Neo Masilo <neolawrencemasilo@gmail.com>
2023-05-06 04:48:48 -0700

    print binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -10,7 +10,7 @@ void print_binary(unsigned long int n)
 {
 	int i, size = sizeof(n), num;
 
-	for (i = (size); 1 >= 0; i--)
+	for (i = (size * 8) - 1; i >= 0; i--)
 	{
 		num = (n >> i) & 1;
 		if (num)

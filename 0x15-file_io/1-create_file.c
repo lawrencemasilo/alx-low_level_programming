c7092d1ecaeefce2af8a6d4100a594509856602d
Neo Masilo <neolawrencemasilo@gmail.com>
2023-05-08 11:16:59 -0700

    create file update

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,14 +15,14 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC , 0600);
 	if (fd == -1)
 	{
 		return (-1);
 	}
 	if (text_content != NULL)
 	{
-		written = write(STDIN_FILENO, text_content, strlen(text_content));
+		written = write(fd, text_content, strlen(text_content));
 		if (written == -1)
 		{
 			return (-1);

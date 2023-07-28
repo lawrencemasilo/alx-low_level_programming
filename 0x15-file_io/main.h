38bc1b08b7e42ff80f5698463640428b9c8d309b
neo masilo <neolawrencemasilo@gmail.com>
2023-07-28 12:41:14 -0700

    copy and write to file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,8 +6,13 @@
 #include <unistd.h>
 #include <string.h>
 #include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+void _cp_content(int file1, int file2);
+void _open_file(char **av);
+void _close(int file1, int file2);
 #endif

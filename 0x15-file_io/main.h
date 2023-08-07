e8e5e84cb148915ce28bd46e9923377eacd0ee7b
neo masilo <neolawrencemasilo@gmail.com>
2023-08-07 11:15:43 -0700

    coping text

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -11,4 +11,7 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+void _close(int file1, int file2);
+void _cp_content(int file1, int file2);
+void _open_file(char **av);
 #endif

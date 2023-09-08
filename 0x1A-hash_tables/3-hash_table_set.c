385a0d457f7fd8a70ab8c2edb626561337784131
Neo Masilo <neolawrencemasilo@gmail.com>
2023-09-08 13:45:24 +0000

    a function that adds element to hash table or adds new node in case of collision

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -59,6 +59,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 			is_same = strcmp(current->key, key);
 			if (is_same == 0)
 			{
+				free(current->value);
 				current->value = strdup(value);
 				if (current->value == NULL)
 					return (0);
